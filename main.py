4923a4e1752209f1908d41f5eac1a4aeb7b299b4
Brisk4t <atharvapcat@gmail.com>
2026-02-21 23:51:12 -0700

    Change deviceid

--- main.py
+++ main.py
@@ -161,6 +161,6 @@ if __name__ == "__main__":
     if button.value() == 0:
         print("Safe mode: skipping main")
     else:
-        app = VirtualPetApp(device_id=0)
+        app = VirtualPetApp(device_id=1)
         app.run()
 
